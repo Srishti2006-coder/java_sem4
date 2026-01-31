72f00aebc2e4159518940a03e4032c8aa86c1f70
Srishti2006-coder <srishtibansal505@gmail.com>
2026-01-31 13:36:14 +0530

    print 12345

--- recursion1.java
+++ recursion1.java
@@ -15,22 +15,54 @@
 //     }
 // }
 
+// class Main { // function call stack
+//     static void mesg() {
+//         System.out.println("hello");
+//         mesg1();
+//     }
+
+//     static void mesg1() {
+//         System.out.println("hi");
+//         mesg2(); // function is calling another function
+//     }
+
+//     static void mesg2() {
+//         System.out.println("hey");
+//     }
+
+//     public static void main(String args[]) {
+//         mesg();
+//     }
+// }
+
+// q.) print no's 1 2 3 4 5 BY USING FUNCTION CALL STACK
+
 class Main {
-    static void mesg() {
-        System.out.println("hello");
-        mesg1();
+    static void print1(int n) {
+        System.out.println(n);
+        print2(n + 1); // print2(2)
     }
 
-    static void mesg1() {
-        System.out.println("hi");
-        mesg2();
+    static void print2(int n) {
+        System.out.println(n);
+        // print3(n + 1); // print3(3)
     }
 
-    static void mesg2() {
-        System.out.println("hey");
-    }
+    // static void print3(int n) {
+    // System.out.println(n);
+    // print4(n + 1); // print4(4)
+    // }
+
+    // static void print4(int n) {
+    // System.out.println(n);
+    // print5(n + 1);
+    // }
+
+    // static void print5(int n) {
+    // System.out.println(n);
+    // }
 
     public static void main(String args[]) {
-        mesg();
+        print1(1);
     }
 }
