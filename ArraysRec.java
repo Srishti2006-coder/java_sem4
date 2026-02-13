05c437755c6ce62663bf26e71d72aee95c17e734
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-13 18:16:28 +0530

    find target el and return it's index

--- ArraysRec.java
+++ ArraysRec.java
@@ -31,32 +31,32 @@
 // 2.) checks if the target el is equal to which index
 
 class Main {
-    // static int find(int arr[], int target, int index) {
-    // if (index == arr.length) {
-    // return -1;
-    // }
-    // if (arr[index] == target) {
-    // return index; // jis bhi idx pr vo element hai
-    // } else {
-    // return find(arr, target, index + 1);
-    // }
-    // }
-
-    static boolean findindex(int arr[], int target, int index) {
-        if (index == arr.length - 1) {
-            return false;
+    static int find(int arr[], int target, int index) {
+        if (index == arr.length) {
+            return -1;
         }
         if (arr[index] == target) {
-            return true;
+            return index; // jis bhi idx pr vo element hai
         } else {
-            return findindex(arr, target, index + 1);
+            return find(arr, target, index + 1);
         }
     }
+
+    // static boolean findindex(int arr[], int target, int index) {
+    // if (index == arr.length - 1) {
+    // return false;
+    // }
+    // if (arr[index] == target) {
+    // return true;
+    // } else {
+    // return findindex(arr, target, index + 1);
+    // }
+    // }
 
     public static void main(String args[]) {
         int arr[] = { 2, 56, 100, 190, 89 };
         int target = 1000;
-        boolean result = findindex(arr, target, 0);
+        int result = find(arr, target, 0);
         System.out.println("found at index : " + result);
     }
 }
