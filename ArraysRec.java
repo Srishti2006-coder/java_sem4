5b9d042ceccc3dca1e7f3bda0197f2e6267772a7
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-13 18:29:06 +0530

    when return type is void

--- ArraysRec.java
+++ ArraysRec.java
@@ -30,17 +30,19 @@
 
 // 2.) checks if the target el is equal to which index
 
+import java.util.ArrayList;
+
 class Main {
-    static int find(int arr[], int target, int index) {
-        if (index == arr.length) {
-            return -1;
-        }
-        if (arr[index] == target) {
-            return index; // jis bhi idx pr vo element hai
-        } else {
-            return find(arr, target, index + 1);
-        }
-    }
+    // static int find(int arr[], int target, int index) {
+    // if (index == arr.length) {
+    // return -1;
+    // }
+    // if (arr[index] == target) {
+    // return index; // jis bhi idx pr vo element hai
+    // } else {
+    // return find(arr, target, index + 1);
+    // }
+    // }
 
     // static boolean findindex(int arr[], int target, int index) {
     // if (index == arr.length - 1) {
@@ -53,10 +55,22 @@ class Main {
     // }
     // }
 
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    static void findAllIndex(int arr[], int target, int index) {
+        if (index == arr.length) {
+            return;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
+    }
+
     public static void main(String args[]) {
         int arr[] = { 2, 56, 100, 190, 89 };
-        int target = 1000;
-        int result = find(arr, target, 0);
-        System.out.println("found at index : " + result);
+        int target = 190;
+        findAllIndex(arr, target, 0);
+        System.out.println(list);
     }
 }
