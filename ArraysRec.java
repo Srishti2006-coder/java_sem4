6c8fe1f056205b4ff8a8e0e99084120d33cf92a4
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-13 17:35:08 +0530

    linearsearch

--- ArraysRec.java
+++ ArraysRec.java
@@ -1,19 +1,27 @@
 
-import java.util.*;
+// import java.util.*;
 
 class Codechef {
+    // static boolean sorted(int arr[], int idx) {
+    // if (idx == arr.length - 1) {
+    // return true;
+    // }
+    // return arr[idx] < arr[idx + 1] && sorted(arr, idx + 1); // next idx se check
+    // }
+
     static boolean sorted(int arr[], int idx) {
         if (idx == arr.length - 1) {
             return true;
         }
-        return arr[idx] < arr[idx + 1] && sorted(arr, idx + 1); // next idx se check
+
+        if (arr[idx] >= arr[idx + 1]) {
+            return false;
+        }
+        return sorted(arr, idx + 1);
     }
 
     public static void main(String args[]) {
-        // int arr[] = { 1, 20, 4, 6, 10, 90, 100 , 101};
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
+        int arr[] = { 12, 300, 890 };
         System.out.println(sorted(arr, 0));
     }
 }
