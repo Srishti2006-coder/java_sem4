aaefdb2793d3c7d15d17a3bead23d3a23e8ae071
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-18 22:57:54 +0530

    push changes

--- ArraysRec.java
+++ ArraysRec.java
@@ -164,6 +164,10 @@ import java.util.*;
 
 class Main {
     public static void main(String args[]) {
-        System.out.println("Hello world Hy ");
+        // System.out.println("Hello world Hy ");
+        int a = 10;
+        int b = 90;
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
