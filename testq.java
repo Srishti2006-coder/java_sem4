dc7b0fc7e737f5080b777c93e0335f2bfa9ca539
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-19 20:52:25 +0530

    add all changes

--- testq.java
+++ testq.java
@@ -3,20 +3,40 @@ import java.util.*;
 
 // 1.) Factorial using recursion 
 
+// class Main {
+//     static int factorial(int n) {
+//         if (n == 0 || n == 1) {
+//             return 1;
+//         } else {
+//             return n * factorial(n - 1);
+//         }
+//     }
+
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         int result = factorial(n);
+//         System.out.println(result);
+//     }
+// }
+
+// 2.) Fibonnacci
+
 class Main {
-    static int factorial(int n) {
-        if (n == 0 || n == 1) {
+    static int fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
             return 1;
-        } else {
-            return n * factorial(n - 1);
-        }
+
+        return fib(n - 1) + fib(n - 2);
     }
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int result = factorial(n);
-        System.out.println(result);
+        System.out.println(fib(n));
     }
 }
