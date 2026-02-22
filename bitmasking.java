e37c15b5f2357893051fb0af05d38db5bad6968c
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-22 23:39:11 +0530

    Complement of a number

--- bitmasking.java
+++ bitmasking.java
@@ -168,22 +168,36 @@
 
 // ques 11. - power set
 
-/*
- * class Main{
- * public static void main(String[] args) {
- * int[] arr = {1,2,3};
- * int n = arr.length;
- * 
- * for(int i=0;i<(1<<n);i++)
- * {
- * for(int j=0;j<n;j++)
- * {
- * if((i&(1<<j))!=0){
- * System.out.print(arr[j]+" ");
- * }
- * }
- * System.out.println();
- * }
- * }
- * }
- */
+/*class Main {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3 };
+        int n = arr.length;
+
+        for (int i = 0; i < (1 << n); i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    System.out.print(arr[j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+// ques 12. Complement of a number 
+
+class Main {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        int bits = Integer.toBinaryString(n).length();
+
+        int mask = (1 << bits) - 1;
+
+        int result = mask ^ n;
+
+        System.out.println(result);
+
+    }
+}
