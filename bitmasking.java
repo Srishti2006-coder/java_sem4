fc4df9c1bf7718f468f76b106007c96d468043b9
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-22 22:50:07 +0530

    XOR operation

--- bitmasking.java
+++ bitmasking.java
@@ -1,24 +1,43 @@
 
 // q.> 1. And Operator (&)
 
-/*class Main {
-    public static void main(String args[]) {
-        int a = 5;
-        int b = 8;
-
-        int result = a & b;
-        System.out.println("And of a & b : " + result);
-    }
-}   */
+/*
+ * class Main {
+ * public static void main(String args[]) {
+ * int a = 5;
+ * int b = 8;
+ * 
+ * int result = a & b;
+ * System.out.println("And of a & b : " + result);
+ * }
+ * }
+ */
 
 // q.) 2. OR Operator (|)
 
-class Main {
-    public static void main(String args[]) {
-        int a = 5;
-        int b = 8;
+/*
+ * class Main {
+ * public static void main(String args[]) {
+ * int a = 5;
+ * int b = 8;
+ * 
+ * int result = a | b;
+ * System.out.println("And of a & b : " + result);
+ * }
+ * }
+ */
 
-        int result = a | b;
-        System.out.println("And of a & b : " + result);
+// q.) 3. XOR Operator (^)
+
+class Main {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+
+        int result = a ^ b;
+
+        System.out.println("XOR = " + result);
+
     }
 }
