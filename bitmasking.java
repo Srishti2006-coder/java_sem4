2588ac874e1186b896c85d7e32e3b6e384844b29
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-22 22:52:02 +0530

    Not operation

--- bitmasking.java
+++ bitmasking.java
@@ -29,15 +29,31 @@
 
 // q.) 3. XOR Operator (^)
 
+/*
+ * class Main {
+ * public static void main(String[] args) {
+ * 
+ * int a = 5;
+ * int b = 3;
+ * 
+ * int result = a ^ b;
+ * 
+ * System.out.println("XOR = " + result);
+ * 
+ * }
+ * }
+ */
+
+// q.) 4. NOT Operator (~)
+
 class Main {
     public static void main(String[] args) {
 
         int a = 5;
-        int b = 3;
 
-        int result = a ^ b;
+        int result = ~a;
 
-        System.out.println("XOR = " + result);
+        System.out.println("NOT = " + result);
 
     }
 }
