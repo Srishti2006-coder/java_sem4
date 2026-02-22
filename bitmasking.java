08ea7e4a53f86f6550ae537d10085a83abfe620a
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-22 23:28:46 +0530

    miising number in an array

--- bitmasking.java
+++ bitmasking.java
@@ -134,7 +134,7 @@
 
 // q.) 9. find unique element
 
-class Main {
+/*class Main {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 3, 5, 2, 5, 1 };
         int ans = 0;
@@ -143,4 +143,43 @@ class Main {
         }
         System.out.println(ans);
     }
+} */
+
+// q.) 10. //ques - find missing number in array
+
+class Main {
+    public static void main(String[] args) {
+        int n = 5;// length of array
+        int[] arr = { 0, 1, 3, 4, 5 };// start from 0 to n but 2 is missing
+        int ans = 0;
+        for (int i = 0; i <= n; i++) {
+            ans ^= i;
+        }
+        for (int num : arr) {
+            ans ^= num;
+        }
+        System.out.print(ans);
+    }
 }
+
+// ques 11. - power set
+
+/*
+ * class Main{
+ * public static void main(String[] args) {
+ * int[] arr = {1,2,3};
+ * int n = arr.length;
+ * 
+ * for(int i=0;i<(1<<n);i++)
+ * {
+ * for(int j=0;j<n;j++)
+ * {
+ * if((i&(1<<j))!=0){
+ * System.out.print(arr[j]+" ");
+ * }
+ * }
+ * System.out.println();
+ * }
+ * }
+ * }
+ */
