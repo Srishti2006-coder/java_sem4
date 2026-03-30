b4a844bb63d5e02d10af684981b3dd50b45aa362
BLACKBOXAI <blackboxai@example.com>
2026-03-30 15:22:02 +0530

    Addition of 2 int no,s , 3 int no, and 2 double no,s using method overloading

--- opps.java
+++ opps.java
@@ -201,92 +201,95 @@
 // }
 // }
 
-// 5.) Set value example 
+// 5.) Set value example
 
 // class DummyClass {
 
-//     int length, width;
+// int length, width;
 
-//     void setValues(int l, int w) {
-//         length = l;
-//         width = w;
-//     }
+// void setValues(int l, int w) {
+// length = l;
+// width = w;
+// }
 
-//     int area() {
-//         return (length * width);
-//     }
+// int area() {
+// return (length * width);
+// }
 // }
 
 // class Main {
-//     public static void main(String[] args) {
-//         DummyClass o1 = new DummyClass(); // o1 is an object of class
-//         o1.setValues(5, 18); // Values passes to object o1
-//         System.out.println(o1.area());
-//     }
+// public static void main(String[] args) {
+// DummyClass o1 = new DummyClass(); // o1 is an object of class
+// o1.setValues(5, 18); // Values passes to object o1
+// System.out.println(o1.area());
+// }
 // }
 
-// 6.) Constructor example without set value 
+// 6.) Constructor example without set value
 
 // class DummyClass {
 
-//     int length, width;
+// int length, width;
 
-//     DummyClass(int l, int w) {
-//         length = l;
-//         width = w;
-//     }
+// DummyClass(int l, int w) {
+// length = l;
+// width = w;
+// }
 
-//     int area() {
-//         return (length * width);
-//     }
+// int area() {
+// return (length * width);
+// }
 // }
 
 // class Main {
-//     public static void main(String[] args) {
-//         DummyClass o1 = new DummyClass(4, 5); // Pass values as parametrized constructor
-//         // o1.setValues(5, 18); // Values passes to object o1
-//         System.out.println(o1.area());
-//     }
+// public static void main(String[] args) {
+// DummyClass o1 = new DummyClass(4, 5); // Pass values as parametrized
+// constructor
+// // o1.setValues(5, 18); // Values passes to object o1
+// System.out.println(o1.area());
+// }
 // }
 
 // 7.) Constructor overloading example
 
-class Student {
-    String name;
-    int age;
-    double marks;
+// class Student {
+// String name;
+// int age;
+// double marks;
 
-    Student() {
-        name = "Srishti";
-        age = 20;
-        marks = 8.5;
-    }
+// Student() {
+// name = "Srishti";
+// age = 20;
+// marks = 8.5;
+// }
 
-    Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+// Student(String name, int age) {
+// this.name = name;
+// this.age = age;
+// }
 
-    Student(String name, int age, double marks) {
-        this.name = name;
-        this.age = age;
-        this.marks = marks;
-    }
+// Student(String name, int age, double marks) {
+// this.name = name;
+// this.age = age;
+// this.marks = marks;
+// }
 
-    void display() {
-        System.out.println("Name is : " + name);
-        System.out.println("Age is : " + age);
-        System.out.println("Marks is : " + marks);
-    }
+// void display() {
+// System.out.println("Name is : " + name);
+// System.out.println("Age is : " + age);
+// System.out.println("Marks is : " + marks);
+// }
 
-    public static void main(String args[]) {
-        Student s1 = new Student();
-        Student s2 = new Student("cv", 88);
-        Student s3 = new Student("Shreya", 20, 8.5);
-        s1.display();
-        System.out.println("\n");
-        s2.display();
-        System.out.println("\n");
-        s3.display();
-    }
-}
+// public static void main(String args[]) {
+// Student s1 = new Student();
+// Student s2 = new Student("cv", 88);
+// Student s3 = new Student("Shreya", 20, 8.5);
+// s1.display();
+// System.out.println("\n");
+// s2.display();
+// System.out.println("\n");
+// s3.display();
+// }
+// }
+
+// 8.) Addition of
