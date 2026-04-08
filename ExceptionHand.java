bfe87442fb629f908b800889a569117b3d8cde1a
BLACKBOXAI <blackboxai@example.com>
2026-04-08 20:17:47 +0530

    single inheritance

--- ExceptionHand.java
+++ ExceptionHand.java
@@ -495,20 +495,35 @@
 
 // 19.) /* Nested try */
 
-class Main {
-    public static void main(String args[]) {
-        try {
-            try {
-                int a[] = { 10, 20, 30 };
-                System.out.println(a[2]);
-            } catch (ArrayIndexOutOfBoundsException a) {
-                System.out.println(a);
-            }
-            System.out.println(10 / 0);
-        } catch (ArithmeticException e) {
-            System.out.println(e); // java.lang.ArithmeticException: / by zero
-            System.out.println("hellooo");
-        }
-        System.out.println("Learn coding");
-    }
-}
+// class Main {
+// public static void main(String args[]) {
+// try {
+// try {
+// int a[] = { 10, 20, 30 };
+// System.out.println(a[2]);
+// } catch (ArrayIndexOutOfBoundsException a) {
+// System.out.println(a);
+// }
+// System.out.println(10 / 0);
+// } catch (ArithmeticException e) {
+// System.out.println(e); // java.lang.ArithmeticException: / by zero
+// System.out.println("hellooo");
+// }
+// System.out.println("Learn coding");
+// }
+// }
+
+// 20.) throw keyword
+
+// class Main {
+// public static void main(String[] args) {
+
+// int age = 16;
+
+// if (age < 18) {
+// throw new ArithmeticException("Not eligible to vote");
+// }
+
+// System.out.println("You can vote");
+// }
+// }
---
+++ Inheritance.java
@@ -0,0 +1,22 @@
+
+class Rectangle {
+    void area(int l, int b) {
+        int area = l * b;
+        System.out.println("Area :  " + area);
+    }
+}
+
+class Recatngle2 extends Rectangle {
+    void perimeter(int l, int b) {
+        int perimeter = 2 * (l + b);
+        System.out.println("Perimeter is : " + perimeter);
+    }
+}
+
+class Main {
+    public static void main(String args[]) {
+        Recatngle2 r1 = new Recatngle2();
+        r1.area(4, 5);
+        r1.perimeter(10, 20);
+    }
+}
