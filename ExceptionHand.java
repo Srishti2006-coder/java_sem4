26a0daef97a95fbe814825e614494ff094903f66
BLACKBOXAI <blackboxai@example.com>
2026-04-08 10:59:57 +0530

    throw keyword

--- ExceptionHand.java
+++ ExceptionHand.java
@@ -493,8 +493,22 @@
 // }
 // }
 
+// 19.) /* Nested try */
+
 class Main {
     public static void main(String args[]) {
-
+        try {
+            try {
+                int a[] = { 10, 20, 30 };
+                System.out.println(a[2]);
+            } catch (ArrayIndexOutOfBoundsException a) {
+                System.out.println(a);
+            }
+            System.out.println(10 / 0);
+        } catch (ArithmeticException e) {
+            System.out.println(e); // java.lang.ArithmeticException: / by zero
+            System.out.println("hellooo");
+        }
+        System.out.println("Learn coding");
     }
 }
